a4f630c66d05e8c8b3fb8659142272ed38d90721
nalread <nalread@gmail.com>
2016-05-17 13:43:04 +0200

    Update ps1.py
    Final version of part 1/3 of PS1

--- ps1.py
+++ ps1.py
@@ -8,10 +8,10 @@ min_mon_rate = float(raw_input("Enter the minimum monthly payment rate as a deci
  
 for i in range(1, 13):
     print "Month " + str(i)
-    min_mon_pay = min_mon_rate * balance
-    interest_paid = ann_int_rate / 12 * balance
-    principle_paid = min_mon_pay - interest_paid
-    balance = balance - principle_paid
+    min_mon_pay = round(min_mon_rate * balance, 2)
+    interest_paid = round(ann_int_rate / 12 * balance, 2)
+    principle_paid = round(min_mon_pay - interest_paid, 2)
+    balance = round(balance - principle_paid, 2)
     print "Minimal monthly payment: " + str(min_mon_pay)
     print "Principle paid: " + str(principle_paid)
     print "Remaining balance: " + str(balance)
