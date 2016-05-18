7f3fba5b8094f1fac11e93c6ed5e04f7548533c5
nalread <nalread@gmail.com>
2016-05-18 16:02:14 +0200

    Create ps1b.py

---
+++ ps1b.py
@@ -0,0 +1,8 @@
+promptBalance = "Enter the outstanding balance on your credit card: "
+promptRate = "Enter the annual credit card interest rate as decimal: "
+def payOff(balance, annualInterestRate):
+	# print "The balance is: " + str(balance) + ", and the interest rate is: " + str(annualInterestRate) + "."
+	guess = 10.0
+	
+	
+payOff(int(raw_input(promptBalance)), float(raw_input(promptRate)))
