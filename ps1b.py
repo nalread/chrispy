0885ca4425aadd97edd56da12aa342ead3ba3a45
nalread <nalread@gmail.com>
2016-05-19 13:28:57 +0200

    Update ps1b.py
    Complete rework

--- ps1b.py
+++ ps1b.py
@@ -1,14 +1,23 @@
-promptBalance = "Enter the outstanding balance on your credit card: "
-promptRate = "Enter the annual credit card interest rate as decimal: "
-
-def payOff(balance, annualInterestRate):
-	# print "The balance is: " + str(balance) + ", and the interest rate is: " + str(annualInterestRate) + "."
-	guess = 10.0
-	month = 0
-	while balance >= 0:
-		month += 1
-		balance = balance + (balance * (annualInterestRate / 12))
-		balance = balance - guess
-		print balance
-		
-payOff(int(raw_input(promptBalance)), float(raw_input(promptRate)))
+initBalance = float(raw_input("Enter the outstanding balance on your credit card: "))
+initRate = float(raw_input("Enter the annual credit card interest rate as decimal: "))
+ 
+monPay = 0
+monRate = initRate / 12
+balance = initBalance
+ 
+while balance > 0:
+    monPay += 10
+    balance = initBalance
+    numMonths = 0
+    while numMonths < 12 and balance > 0:
+        numMonths += 1
+        interest = balance * monRate
+        balance -= monPay
+        balance += interest
+ 
+balance = round(balance, 2)
+ 
+print "RESULT:"
+print "Monthly payment to pay off the debt in 1 year: ", monPay
+print "Number of months required to pay off the debt: ", numMonths
+print "Balance: ", balance
